4692267aae3eab9ed5d0136bc8154da7f92c0ed4
mtello5 <mtello5@mail.fresnostate.edu>
2021-04-15 16:56:19 -0700

    Added Structured English

---
+++ ISTest1.py
@@ -0,0 +1,13 @@
+"""
+The program is made to determine which method of payment is better (pays more $)
+
+First option pays flat $100 per day for 10 days.
+Second option pays $1 for the first day and doubles amound each day for 10 days.
+
+option1 will output 100 * 10 days
+option2 will loop 10 times, addgit ing to previous total, doubling the amount each time and adding to total
+
+If total amount for each option is equal, output "Option 1 and Option 2 pays the same"
+If option1 pays better, we output "Option 1 is better"
+If option2 pays better, we output "Option 2 is better"
+"""
